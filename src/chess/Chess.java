74184e3b6a8bb1bdea7f02fbd388935fa1d209f9
farah <farah@172.27.55.174>
2017-02-27 21:54:48 -0500

    bh

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -5,6 +5,8 @@ public class Chess {
     public static void main(int argc, String[] argv) {
        System.out.println("Hello");
        System.out.println("Hello");
+       System.out.println("Hello");
+
 
     }
 }
