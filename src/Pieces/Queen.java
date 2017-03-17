fee27556717ea3ebb86167265bf380efc3cb10c1
Kevin <kevin.cochran@rutgers.edu>
2017-03-17 14:20:25 -0400

    Implement Queen class

--- src/Pieces/Queen.java
+++ src/Pieces/Queen.java
@@ -13,13 +13,68 @@ public class Queen extends Piece {
 
 	@Override
 	public void popMoves(List<Board.Square> board) {
-		// TODO write popMoves Queen
+	    validMoves.clear();
+	    /*
+	    The Queen is a combonation of Rook and Bishop class. It has 8 directions
+	    it can move in: up, down, left, right, upLeft, upRight, downLeft, downRight
+	     */
+        Position up[] = new Position[7];
+        Position down[] = new Position[7];
+        Position right[] = new Position[7];
+        Position left[] = new Position[7];
+        Position upRight[] = new Position[7];
+        Position upLeft[] = new Position[7];
+        Position downRight[] = new Position[7];
+        Position downLeft[] = new Position[7];
+        // Populate the possible positions
+        for (Position p : Position.values()) {
+            for (int i = 0; i < 7; i++) { // Fill up positions
+                if (p.coordEquals(current.x, current.y+(i+1)))
+                    up[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill down positions
+                if (p.coordEquals(current.x, current.y-(i+1)))
+                    down[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill left positions
+                if (p.coordEquals(current.x+(i+1), current.y))
+                    left[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill right positions
+                if (p.coordEquals(current.x-(i+1), current.y))
+                    right[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill upRight positions
+                if (p.coordEquals(current.x+(i+1), current.y+(i+1)))
+                    upRight[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill upLeft positions
+                if (p.coordEquals(current.x-(i+1), current.y+(i+1)))
+                    upLeft[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill downRight positions
+                if (p.coordEquals(current.x+(i+1), current.y-(i+1)))
+                    downRight[i] = p;
+            }
+            for (int i = 0; i < 7; i++) { // Fill downLeft positions
+                if (p.coordEquals(current.x-(i+1), current.y-(i+1)))
+                    downLeft[i] = p;
+            }
+        }
+        // Add the valid positions to validMoves list
+        addValid(board,up);
+        addValid(board,down);
+        addValid(board,left);
+        addValid(board,right);
+        addValid(board,upLeft);
+        addValid(board,upRight);
+        addValid(board,downLeft);
+        addValid(board,downRight);
 	}
 
 	@Override
 	public void setCurrent(Position p) {
-	
-		
+	    this.current = p;
 	}
 
 	public String toString() {
